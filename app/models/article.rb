7682d1a9f9407c6664bbf0227fd40de0a04fc817
Yemre <aybey77@gmail.com>
2019-06-19 19:51:58 +0300

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_str)
+    list = tags_str.downcase.scan(/[A-Za-z\d]+/).uniq
+    new_or_found_tags = list.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
